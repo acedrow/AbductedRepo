18db45d8efd2b818a114998bf07dd67c99bff05f
Paul Twomey <twomeypm@beloit.edu>
2015-03-17 22:59:08 -0500

    files to folder

---
+++ TextBasedRPG/src/firstIterationTest/playerClassTypes/Duelist.java
@@ -0,0 +1,11 @@
+package firstIterationTest.playerClassTypes;
+
+import firstIterationTest.PlayClass;
+
+public class Duelist extends PlayClass {
+
+	public Duelist() {
+		super(17, 20, 15, 13, 15, 10);
+	}
+
+}
---
+++ TextBasedRPG/src/firstIterationTest/playerClassTypes/Rogue.java
@@ -0,0 +1,11 @@
+package firstIterationTest.playerClassTypes;
+
+import firstIterationTest.PlayClass;
+
+public class Rogue extends PlayClass {
+
+	public Rogue() {
+		super(13, 20, 17, 13, 17, 10);
+	}
+
+}
